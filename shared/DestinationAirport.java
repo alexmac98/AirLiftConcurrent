1e9d8851fb34de3e722fb126898a572e68311543
Alex <a.machado@ua.pt>
2021-04-05 13:44:56 +0100

    DestinationAirport implemented. Not tested

--- shared/DestinationAirport.java
+++ shared/DestinationAirport.java
@@ -1,17 +1,36 @@
 package shared;
 
+import entities.Pilot;
+import states.PilotState;
+import utils.Log;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DestinationAirport{
 
     private GRI repository;
+    private ReentrantLock mutex;
+    
     public DestinationAirport(GRI repository){
         this.repository = repository;
+        this.mutex = new ReentrantLock();
     }
 
-    
-
     public void flyToDeparturePoint() {
-        
+        Pilot pilot = null;
+        try{
+            this.mutex.lock();
+
+            pilot = (Pilot) (Thread.currentThread());
+            pilot.setState(PilotState.FLYING_BACK);
+
+            Log.print("DestinationAirport", "Pilot is flying back to the departure airport.");
+
+        }catch(Exception e){
+
+        }finally{
+            this.mutex.unlock();
+        }
     }
 
     
