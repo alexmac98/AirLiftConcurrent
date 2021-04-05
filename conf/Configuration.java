1e51c802ee90f322ffdd0a36884eb5b44593cc8f
Alex <a.machado@ua.pt>
2021-04-05 10:40:32 +0100

    Configuration file implemented

---
+++ conf/Configuration.java
@@ -0,0 +1,7 @@
+package conf;
+
+public class Configuration {
+    public static final int NUMBER_OF_PASSENGERS = 21;
+    public static final int PLANE_CAPACITY = 10;
+    public static final int MINIMUM_THRESHOLD_PASSENGERS = 5;
+}
